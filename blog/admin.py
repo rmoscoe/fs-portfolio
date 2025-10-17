2a154d6dbdd2b3344317e49077b45e720e2f8cf0
Ryan Moscoe <ryanmoscoe@gmail.com>
2025-10-17 11:48:35 -0700

    create blog models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,29 @@
+from core_site.admin import BaseModelAdmin, BaseModelWithURLAdmin
 from django.contrib import admin
+from .models import Topic, Post, UniquePageView, Like
 
-# Register your models here.
+class TopicAdmin(BaseModelAdmin):
+    fields = ['name', 'parent', 'show']
+    list_display = ['name', 'parent__name', 'children', 'show', 'posts']
+    search_fields = ['name']
+    
+    def children(self, obj):
+        return '; '.join([child.name for child in obj.children.all()])
+    
+    def posts(self, obj):
+        return '\n'.join([post.title for post in obj.posts.all()])
+
+class PostAdmin(BaseModelWithURLAdmin):
+    fields = ['topic', 'title', 'created_at', 'show', 'hero_image', 'hero_image_url', 'body']
+    list_display = ['topic__name', 'title', 'created_at', 'show']
+    search_fields = ['title', 'body']
+
+class InteractionAdmin(admin.ModelAdmin):
+    fields = ['post', 'ip_address', 'created_at']
+    list_display = ['post', 'ip_address', 'created_at']
+    search_fields = ['post', 'ip_address']
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(UniquePageView, InteractionAdmin)
+admin.site.register(Like, InteractionAdmin)
