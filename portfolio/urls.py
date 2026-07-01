ca08bbf8ac807ff67275ebcb05671731c7958312
Ryan Moscoe <ryanmoscoe@gmail.com>
2026-07-01 14:28:14 -0700

    remove prompt engineering and update About

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,8 @@
 from django.views.decorators.cache import cache_page
 from django.urls import path
-from .views import SoftwareEngineeringView, PromptEngineeringView, InstructionalDesignView
+from .views import SoftwareEngineeringView, InstructionalDesignView
 
 urlpatterns = [
     path('software-engineering/', cache_page(24 * 60 * 60)(SoftwareEngineeringView.as_view()), name='software'),
-    path('ai-prompt-engineering/', cache_page(24 * 60 * 60)(PromptEngineeringView.as_view()), name='ai'),
     path('instructional-design/', cache_page(24 * 60 * 60)(InstructionalDesignView.as_view()), name='instructional-design')
 ]
